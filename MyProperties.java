b0d87b10ab145fd93f43a012b5717739e9e0480e
mabakhtiyari <marjanbakhtiyari@yahoo.com>
2024-05-28 03:01:28 +0430

    practice1

---
+++ MyProperties.java
@@ -0,0 +1,24 @@
+import java.io.StringReader;
+
+public class MyProperties {
+    public static void main(String[] args) {
+        System.out.println("Print Information");
+        String name="Marjan";
+        String family="Bakhtiari";
+        int age=31;
+        String job="System Analyst";
+        long salary=850000000;
+        Boolean single=true;
+        int height=172;
+
+        System.out.println("Name is:"+name);
+        System.out.println("Family is:"+family);
+        System.out.println("Age is:"+age);
+        System.out.println("Job is:"+job);
+        System.out.println("Salary is:"+salary);
+        System.out.println("Im Single");
+        System.out.println("My height is:"+height);
+
+
+    }
+}
