f47187ad7a2c99be5ea809d5d08803256d58b909
mabakhtiyari <marjanbakhtiyari@yahoo.com>
2024-06-17 22:47:49 +0430

    practice1

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(calcFactorial(num));
+    }
+
+    public static int calcFactorial(int n) {
+
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        else {
+            return n * calcFactorial(n - 1);
+        }
+    }
+}
